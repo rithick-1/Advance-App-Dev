46eefddc4062eebc5b856c6cebcbefa185458248
rithick-1 <119099540+rithick-1@users.noreply.github.com>
2024-03-19 15:05:26 +0530

    Add files via upload

---
+++ Day 1/src/components/UI/Footer.jsx
@@ -0,0 +1,11 @@
+import '../../assets/Design/Footer.css'; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Your Yoga Academy. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
